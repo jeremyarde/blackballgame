2b4173b8b30926dcb514958c9399c66bbcb11430
Jeremy <jere.arde@gmail.com>
2024-07-23 19:54:17 -0700

    connecting via ws and dioxus working

--- bb-admin/README.md
+++ bb-admin/README.md
@@ -12,6 +12,7 @@ Run the following command in the root of the project to start the Dioxus dev ser
 
 ```bash
 dx serve --hot-reload
+dx serve --port 5173 --hot-reload
 ```
 
 - Open the browser to http://localhost:8080
--- bb-admin/src/main.rs
+++ bb-admin/src/main.rs
@@ -2,8 +2,11 @@
 
 use std::collections::HashMap;
 
-use common::GameState;
+use common::{Connect, GameEvent, GameMessage, GameState};
 use dioxus::prelude::*;
+use futures_util::{SinkExt, StreamExt, TryStreamExt};
+use reqwest::Client;
+use reqwest_websocket::{Message, RequestBuilderExt};
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use tracing::Level;
@@ -14,16 +17,61 @@ fn main() {
     launch(App);
 }
 
+enum InternalMessage {
+    Game(GameMessage),
+    Server(Connect),
+}
+
+static GAMESTATE: GlobalSignal<GameState> = Signal::global(|| GameState::new());
+
 #[component]
 fn App() -> Element {
+    let mut test_ws = use_signal(|| String::new());
+
+    let ws: Coroutine<InternalMessage> = use_coroutine(|mut rx| async move {
+        // Connect to some sort of service
+        // Creates a GET request, upgrades and sends it.
+        let response = Client::default()
+            .get("ws://0.0.0.0:8080/ws")
+            .upgrade() // Prepares the WebSocket upgrade.
+            .send()
+            .await
+            .unwrap();
+
+        // Turns the response into a WebSocket stream.
+        let mut websocket = response.into_websocket().await.unwrap();
+
+        // wait for connection message
+        let connection_details: InternalMessage = rx.next().await.unwrap();
+
+        match connection_details {
+            InternalMessage::Game(x) => {
+                let msg = Message::Text(json!(x).to_string());
+                websocket.send(msg).await.unwrap();
+            }
+            InternalMessage::Server(x) => {
+                let msg = Message::Text(json!(x).to_string());
+                websocket.send(msg).await.unwrap();
+            }
+        }
+
+        // The WebSocket is also a `TryStream` over `Message`s.
+        while let Some(message) = websocket.try_next().await.unwrap() {
+            if let Message::Text(text) = message {
+                println!("received: {text}");
+                test_ws.set(text);
+            }
+        }
+    });
+
     let mut username = use_signal(|| String::new());
     let mut lobby = use_signal(|| String::new());
     let mut connect_response = use_signal(|| String::from("..."));
-    let mut lobbies = use_signal(|| vec![]);
+    let mut lobbies = use_signal(|| vec![String::from("No lobbies")]);
 
     // Build cool things ✌️
     let mut get_games_future = use_resource(|| async move {
-        reqwest::get("http://localhost:8080/v1/rooms")
+        reqwest::get("http://0.0.0.0:8080/v1/rooms")
             .await
             .unwrap()
             .json::<HashMap<String, GameState>>()
@@ -59,42 +107,14 @@ fn App() -> Element {
     };
 
     let join_lobby = move |_| {
-        #[derive(Deserialize, Serialize)]
-        struct Connect {
-            username: String,
-            channel: String,
-        }
-
-        spawn(async move {
-            let resp = reqwest::Client::new()
-                .post("http://localhost:8080/rooms")
-                .json(&Connect {
-                    username: username().clone(),
-                    channel: lobby().clone(),
-                })
-                .send()
-                .await;
-
-            match resp {
-                Ok(data) => {
-                    // log::info!("Got response: {:?}", resp);
-                    connect_response.set(format!("response: {:?}", data).into());
-                }
-                Err(err) => {
-                    // log::info!("Request failed with error: {err:?}")
-                    connect_response.set(format!("{err}").into());
-                }
-            }
-        });
+        ws.send(InternalMessage::Server(Connect {
+            username: username(),
+            channel: lobby(),
+            secret: None,
+        }));
     };
 
     let refresh_lobbies = move |_| {
-        #[derive(Deserialize, Serialize)]
-        struct Connect {
-            username: String,
-            channel: String,
-        }
-
         spawn(async move {
             let resp = reqwest::Client::new()
                 .get("http://localhost:8080/rooms")
@@ -116,16 +136,19 @@ fn App() -> Element {
 
     rsx! {
         div { class: "flex flex-col",
-            div { "results: {connect_response}" }
+            div { "websocket state: {test_ws}" }
+            label { "username" }
             input {
                 r#type: "text",
                 value: "{username}",
                 oninput: move |event| username.set(event.value())
             }
+            label { "lobby" }
             input {
                 r#type: "text",
                 value: "{lobby}",
-                oninput: move |event| lobby.set(event.value())
+                oninput: move |event| lobby.set(event.value()),
+                "lobby"
             }
         }
         button { onclick: join_lobby, "Join" }
